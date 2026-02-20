d8ef63b6ae3f87458aa9bb8bf129ec2b25514d70
YIRIBET <132949296+YIRIBET@users.noreply.github.com>
2026-02-20 13:18:41 -0600

    UPDATE USERS modal para agregar y actualizar

---
+++ client/src/components/modals/usersModal.jsx
@@ -0,0 +1,169 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { createUser, updateUser } from "../../services/userService";
+import * as Yup from "yup";
+
+const getValidationSchema = (isEdit) =>
+  Yup.object({
+    nombre: Yup.string().required("El nombre es obligatorio"),
+    apellidos: Yup.string().required("Los apellidos son obligatorios"),
+    email: Yup.string()
+      .email("Correo inválido")
+      .required("El correo es obligatorio"),
+    password: isEdit
+      ? Yup.string()
+      : Yup.string()
+          .min(6, "Mínimo 6 caracteres")
+          .required("La contraseña es obligatoria"),
+    media_type_id: Yup.string().required("Selecciona un medio"),
+  });
+
+const UsersModal = ({ user, onClose, onSuccess }) => {
+  const isEdit = Boolean(user);
+
+  const initialValues = {
+    nombre: user?.nombre || "",
+    apellidos: user?.apellidos || "",
+    email: user?.email || "",
+    password: "",
+    media_type_id: user?.media_type_id || "",
+  };
+
+  const handleSubmit = async (values) => {
+    if (isEdit) {
+      await updateUser(user.id, values);
+    } else {
+      await createUser(values);
+    }
+    onSuccess();
+    onClose();
+  };
+  const inputClass = (error, touched) =>
+    `w-full border p-2 rounded-md bg-gray-50 
+   ${
+     touched
+       ? error
+         ? "border-red-500 focus:ring-red-500"
+         : "border-green-500 focus:ring-green-500"
+       : "border-gray-300"
+   }`;
+
+  return (
+    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg p-6 w-96">
+        <div className="flex justify-end items-center">
+        <button
+                  type="button"
+                  onClick={onClose}
+                  className="px-4 py-2 rounded"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+                </button>
+         </div>
+
+        <h2 className="text-xl font-bold mb-6 text-center">
+          {isEdit ? "Editar usuario" : "Crear usuario"}
+        </h2>
+        
+       
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={getValidationSchema(isEdit)}
+          onSubmit={handleSubmit}
+          enableReinitialize
+        >
+          {({ errors, touched, isSubmitting }) => (
+            <Form className="space-y-4">
+              <div className="flex gap-4">
+                <div className="flex flex-col w-1/2">
+                  <Field
+                    name="nombre"
+                    placeholder="Nombre"
+                    className={inputClass(errors.nombre, touched.nombre)}
+                  />
+                  <ErrorMessage
+                    name="nombre"
+                    component="p"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="flex flex-col w-1/2">
+                  <Field
+                    name="apellidos"
+                    placeholder="Apellidos"
+                    className={inputClass(errors.apellidos, touched.apellidos)}
+                  />
+                  <ErrorMessage
+                    name="apellidos"
+                    component="p"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+              </div>
+
+              <Field
+                as="select"
+                name="media_type_id"
+                className={inputClass(
+                  errors.media_type_id,
+                  touched.media_type_id,
+                )}
+              >
+                <option value="">Selecciona un medio</option>
+                <option value="1">Radio</option>
+                <option value="2">Redes Sociales</option>
+                <option value="3">Website</option>
+                <option value="4">TV</option>
+                <option value="5">Periódico</option>
+              </Field>
+              <ErrorMessage
+                name="media_type_id"
+                component="p"
+                className="text-red-500 text-sm"
+              />
+
+              <Field
+                name="email"
+                type="email"
+                placeholder="Email"
+                className={inputClass(errors.email, touched.email)}
+              />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className="text-red-500 text-sm"
+              />
+
+              <Field
+                name="password"
+                type="password"
+                placeholder="Contraseña"
+                className={inputClass(errors.password, touched.password)}
+              />
+              <ErrorMessage
+                name="password"
+                component="p"
+                className="text-red-500 text-sm"
+              />
+
+              <div className="justify-center items-center flex gap-4">
+                
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="px-4 py-2 bg-[#1A6795] text-white rounded"
+                >
+                  {isEdit ? "Guardar" : "Crear"}
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default UsersModal;
--- client/src/views/Admin/Users.jsx
+++ client/src/views/Admin/Users.jsx
@@ -1,28 +1,51 @@
 import React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchUsers } from "../../services/userService";
+import UsersModal from "../../components/modals/usersModal";
 
 function Users() {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
 
-    useEffect(() => {
-        async function loadUsers() {
-            try {
-                const data = await fetchUsers();
-                setUsers(data);
-            } catch (error) {
-                console.error("Error fetching users:", error);
-            }
-        }
-        loadUsers();
-    }, []);
-    
+  useEffect(() => {
+    async function loadUsers() {
+      try {
+        const data = await fetchUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    }
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const data = await fetchUsers();
+    setUsers(data);
+  };
+
+  const openCreateModal = () => {
+    setSelectedUser(null);
+    setIsModalOpen(true);
+  };
+
+
+  const openModal = (user) => {
+    setSelectedUser(user);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedUser(null);
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
       <h1 className="text-3xl font-bold mb-6">Usuarios</h1>
       <div className="flex justify-end items-center mb-4 mr-4">
-        <button className="bg-[#1A6795] text-white px-4 py-2 rounded-md ml-2">
+        <button className="bg-[#1A6795] text-white px-4 py-2 rounded-md ml-2" onClick={openCreateModal}>
           Agregar
         </button>
       </div>
@@ -49,7 +72,6 @@ function Users() {
               className="bg-gray-100 rounded-md pl-10 pr-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
-          
         </div>
         <table className="w-full bg-white rounded-lg shadow-md">
           <thead>
@@ -61,24 +83,65 @@ function Users() {
             </tr>
           </thead>
           <tbody>
-                
-            {users.map(user => (
+            {users.map((user) => (
               <tr key={user.id} className="border-t border-gray-200">
                 <td className="py-2 px-4">{user.nombre}</td>
                 <td className="py-2 px-4">{user.email}</td>
                 <td className="py-2 px-4">{user.type_name}</td>
                 <td className="py-2 px-4">
-                <button className=" hover:text-blue-700 mr-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
-                </button>
-                <button className="text-red-500 hover:text-red-700">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
-                </button>
-              </td>
-            </tr>
+                  <button
+                    className=" hover:text-blue-700 mr-2"
+                    onClick={() => openModal(user)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-pencil-icon lucide-pencil"
+                    >
+                      <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                      <path d="m15 5 4 4" />
+                    </svg>
+                  </button>
+
+                  <button className="text-red-500 hover:text-red-700">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="lucide lucide-trash2-icon lucide-trash-2"
+                    >
+                      <path d="M10 11v6" />
+                      <path d="M14 11v6" />
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                      <path d="M3 6h18" />
+                      <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
+        {isModalOpen && (
+        <UsersModal
+          user={selectedUser}
+          onClose={closeModal}
+          onSuccess={loadUsers}
+        />
+      )}
       </div>
     </div>
   );
